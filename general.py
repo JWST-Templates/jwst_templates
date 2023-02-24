b3c2021e2534adc586bbe75d574037e58ff4e220
janerigby <jane.rigby@gmail.com>
2023-02-24 14:31:35 -0500

    where redshifts come from

--- general.py
+++ general.py
@@ -7,11 +7,14 @@ def get_targnames():
     # Let's use common nomenclature everywhere to make life easier
     return(('SGAS1723+34', 'SGAS1226+21', 'SPT2147-50', 'SPT0418-47'))
 
-def get_redshifts():
+def get_redshifts(kind='nebular'):
     names = get_targnames()
-    redshifts = [1.3293, 2.9252, 3.76, 4.22] # these last 2 need more precision
+    if kind == 'nebular' :
+        redshifts = [1.3293, 2.9260, 3.76, 4.22] # these last 2 need more precision
+    elif kind == 'stellar'
+        redshifts = [1.3293, 2.9252, 3.76, 4.22]
     # Origin of these redshifts:
     #    SGAS1723: Rigby et al. 2021
-    #    SGAS1226: Rigby et al. 2018
+    #    SGAS1226: Rigby et al. 2018 
     # 
     return( dict(zip(names, redshifts)))
