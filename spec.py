7dcfa99ee51ede2bd8018db714b04dfc64046dc6
janerigby <jane.rigby@gmail.com>
2023-02-27 11:36:19 -0500

    Added code to grab dispersion (micron/pixel) and spectral resolution R from Jdox files now in jwst_templates/Reference_files/

--- spec.py
+++ spec.py
@@ -16,13 +16,13 @@ from astropy.nddata import StdDevUncertainty
 
 import os
 
-
 def load_nirspec_dispersion(grating) : #grating as in 'prism', 'g140m', 'g235h' 
-    ndir = '/Users/jrrigby1/Python/TEMPLATES/jwst_templates/Reference_files/' # this is hardcoded.  Help having it look at the module?
+    ndir = '/Users/jrrigby1/Python/TEMPLATES/jwst_templates/Reference_files/' # this is hardcoded.
+    # I need help getting the above line to look inside the module's Reference_files/
     Rfilename = 'jwst_nirspec_' + grating.lower() + '_disp.fits'
     RR, Rheader = fits.getdata(ndir + Rfilename, header=True)
+    # WAVELENGTH will be wavelength in micron, DLDS will be dispersion in microns per pixel; R is dimensionless spectral resolution
     return(RR, Rheader)
-
 
 # some specific stuff for JWST IFU spectra:
 
